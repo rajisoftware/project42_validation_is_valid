80e7f825497f69f59adf826fe52a07ee5bbfbfdd
rajyalakshmi <rajianna239@gmail.com>
2023-04-27 00:06:25 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms 
+from django.core import validators 
 def check_for_a(value):
     if value[0]=='a':
         raise forms.ValidationError('should not start with a')
@@ -7,22 +8,23 @@ def check_for_len(value):
         raise forms.ValidationError('less than 5 character')
 
 class studentForm(forms.Form):
-    name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
+    name=forms.CharField(max_length=100,validators=[check_for_a,validators.MaxLengthValidator(5)])
     age=forms.IntegerField()
     email=forms.EmailField()
     re_enter_email=forms.EmailField()
-    #botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+    mobile=forms.CharField(max_length=10,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
 
     def clean(self):
         e=self.cleaned_data['email']
         r=self.cleaned_data['re_enter_email']
         if e!=r:
             raise forms.ValidationError('not matched')
-    def clean_age(self):
-       a=self.cleaned_data['age']
-       if a<18:
-           raise forms.ValidationError('less than 18')#
-  #  def clean_botcatcher(self):
-       # bot=self.cleaned_data['botcatcher']
-      #  if len(bot)>5:
-          #  raise forms.ValidationError('not validate')#
+    #def clean_age(self):
+       #a=self.cleaned_data['age']
+       #if a<18:
+          #raise forms.ValidationError('less than 18')
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>5:
+            raise forms.ValidationError('not validate')
